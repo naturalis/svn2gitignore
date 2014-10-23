1a160ffd02dea99f9685cd0c6798421b7fd4c1ac
David Heijkamp <david.heijkamp@naturalis.nl>
2014-10-23 09:32:14 +0200

    First working version

---
+++ svn2gitignore.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+import sys
+import os
+import subprocess
+
+def svn_propget_svnignore(path):
+    '''fetch the svn:ignore property of given path'''
+    p = subprocess.Popen(['svn', 'propget', 'svn:ignore', '-R', path], stdout=subprocess.PIPE)
+    p.wait()
+    data = p.stdout.read().strip()
+    return data
+
+
+def svnignore_to_gitignore(path, data):
+    gitignore = path + '/.gitignore'
+    if os.path.isfile(gitignore):
+        os.rename(gitignore, gitignore + '.orig')
+    f = open(gitignore, 'w')
+
+    dirs = data.split('\n\n')
+
+    for dir in dirs:
+        subdir = dir.replace(' - ','\n').split('\n')
+        if subdir[0] == '.':
+            for i in subdir[1:]:
+                f.write(i + '\n')
+        elif subdir[0] == '':
+            continue
+        else:
+            for i in subdir[1:]:
+                f.write(subdir[0] + '/' + i + '\n')
+    f.close()
+
+
+def main():
+
+    if len(sys.argv) < 2:
+        print 'Usage: %s <repo path>' % sys.argv[0]
+        sys.exit()
+
+    for path in sys.argv[1:]:
+        dirpath, filename = os.path.split(path)
+        svnignore_data = svn_propget_svnignore(dirpath)
+
+    svnignore_to_gitignore(path, svnignore_data)
+
+if __name__ == '__main__':
+    main()
