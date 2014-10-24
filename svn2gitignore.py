473d85abe6049680ace5354bc2126e6b7ef7c7a4
David Heijkamp <david.heijkamp@naturalis.nl>
2014-10-24 09:22:16 +0200

    Cosmetic changes

--- svn2gitignore.py
+++ svn2gitignore.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-#
+
 import sys
 import os
 import subprocess
 
+
 def svn_propget_svnignore(path):
     '''fetch the svn:ignore property of given path'''
-    p = subprocess.Popen(['svn', 'propget', 'svn:ignore', '-R', path], stdout=subprocess.PIPE)
+    p = subprocess.Popen(['svn', 'propget',
+                          'svn:ignore', '-R',
+                          path], stdout=subprocess.PIPE)
     p.wait()
     data = p.stdout.read().strip()
     return data
@@ -22,7 +25,7 @@ def svnignore_to_gitignore(path, data):
     dirs = data.split('\n\n')
 
     for dir in dirs:
-        subdir = dir.replace(' - ','\n').replace(path,'').replace(path[:-1],'.').split('\n')
+        subdir = dir.replace(' - ', '\n').replace(path, '').replace(path[:-1], '.').split('\n')
         print subdir
         if subdir[0] == '.':
             for i in subdir[1:]:
@@ -49,6 +52,7 @@ def main():
         svnignore_data = svn_propget_svnignore(dirpath)
 
     svnignore_to_gitignore(path, svnignore_data)
+
 
 if __name__ == '__main__':
     main()
